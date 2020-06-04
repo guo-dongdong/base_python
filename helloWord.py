d248a8941a0b4862c9dbff91f142c2fbbf711c60
guodongdong <1097765642@qq.com>
2020-06-04 14:23:54 +0800

    新增helloword

---
+++ helloWord.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @CreateTime    : 2020-06-04 14:23
+# @Author  : guoDD
+# @Email   : Email
+# @File    : helloWord
+
+"""
+	
+"""
+print("helloword")
