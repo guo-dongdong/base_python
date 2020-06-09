c8fbc5820b9204f23bc124aafa053c1e93ad8c56
guodongdong <1097765642@qq.com>
2020-06-09 09:34:23 +0800

    优化test

--- Try/test/testCar.py
+++ Try/test/testCar.py
@@ -30,44 +30,46 @@ version=[]
 remarks=[]
 
 
-# def Extract_value(i_list,j_list,fill_list):
-#     for i in i_list:
-#         for j in j_list:
-#             if i in j:
-#                 fill_list.append(i)
-#                 j = j.strip(i)
-#             else:
-#                 fill_list.append("null")
-#     return  i_list,j_list,fill_list
-#
-#
-# Extract_value(product_list,line,listPro)
-#
-# # Extract_value(business_list,line,listBus)
-for x in line:
-    ver = re.findall(r'\d+\.(?:\d+\.)*\d+',x)
-    version_list.append(ver)
-# print(version_list)
-
-
-for i in product_list:
-    for j in line:
-        if i in j:
+for email_Subject in line:
+    # 提取产品
+    for i in product_list:
+        # 判断产品线列表中的值是否在邮件主题中，有的话，插入到listPro=[]里，并结束循环
+        if i in email_Subject:
             listPro.append(i)
-            for n in business_list:
-                if n in j.strip(i):
-                    listBus.append(n)
-                    for v in version_list:
-                        if str(v) in j.strip(i).strip(n) and len(v)>0:
-                            version.append(v)
-                            k = j.strip(i).strip(n).strip(v)
-                            # remarks.append(k)
-                            print(k)
-                            product_list.remove(j)
+            break
+    # 判断如果所有产品线列表中的值都没有在邮件主题中出现，那就把null插入到插入到listPro[]
+    if i not in email_Subject:
+        listPro.append("null")
 
+    # 提取业务线（逻辑和提取产品线一样）
+    for j in business_list:
+        # 判断产品线列表中的值是否在邮件主题（邮件主题中删除了产品线）中，有的话，插入到listBus=[]里，并结束循环
+        if j in email_Subject.replace(i, ""):
+            listBus.append(j)
+            break
 
+    # 判断如果所有业务线列表中的值都没有在邮件主题中出现，那就把null插入到插入到listBus[]
+    if j not in email_Subject.replace(i, ""):
+        listBus.append("null")
+
+    # 版本号
+    v = re.findall(r'\d+\.(?:\d+\.)*\d+', email_Subject.replace(i, "").replace(j, ""))
+    ver = ''.join(v)
+    # 如果没有找到版本号，就version=[]中插入null；否则插入ver
+    if ver == "":
+        version.append("null")
+    else:
+        version.append(ver)
+
+    # 邮件标题中，删除产品、业务、版本号。再删除其他一些没用的信息，其他的就是备注
+    remarks.append(email_Subject.replace(i, "").replace(j, "").replace(ver, "").replace("回复：", "").replace("【项目上线】", "")
+                   .replace("-", "").replace("_", "").replace("【数据库上线】", "").replace("v", "").replace("V", ""))
 
 print(listPro)
 print(listBus)
-
+print(version)
 print(remarks)
+print(len(listPro))
+print(len(listBus))
+print(len(version))
+print(len(remarks))
